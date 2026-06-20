476bc95b35674b5be796bd25adcc65f7c1a88b36
arko92 <arkobhattacharjee92@gmail.com>
2026-06-20 08:14:01 -0400

    Better linting

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,7 +139,6 @@ class TestAccountService(TestCase):
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
@@ -170,7 +169,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
-
 
     def test_delete_account(self):
         """It should Delete an Account"""
